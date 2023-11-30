17b195029e2decd570c7cf5e0f967fc181f2eab7
Eric Chen <kych@berkeley.edu>
2023-11-30 02:08:20 +0000

    align and run basic pubsub

--- fogros2-rt-x/setup.py
+++ fogros2-rt-x/setup.py
@@ -48,6 +48,7 @@ setup(
     entry_points={
         "console_scripts": [
             "replayer = fogros2_rt_x.replayer:main",
+            "recorder = fogros2_rt_x.recorder:main",
         ],
     },
 )
