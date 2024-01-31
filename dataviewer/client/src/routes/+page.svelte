15dc8c3596ccaaf63e607a1f6932cc1377490bf0
Trinity Chung <trinityjchung@gmail.com>
2024-01-31 03:16:36 -0800

    add sorting, update value, viewer settings

--- dataviewer/client/src/routes/+page.svelte
+++ dataviewer/client/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-  import { onMount } from "svelte";
-  import Modal from "$lib/Modal.svelte";
+  import { browser } from "$app/environment";
   import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
   import Fa from "svelte-fa";
+  import Modal from "$lib/Modal.svelte";
+  import Tags from "svelte-tags-input";
+
   const server = "http://localhost:5000";
 
   let files = [];
@@ -13,15 +15,29 @@
   let columns = [];
   let columns_toggle = {};
 
-  let savedTempValue = "";
-  let updateModalActive = false;
-  let deleteModalActive = false;
-  let exportModalActive = false;
-  let modalText = "";
-  let exportOptions = {};
+  let row_count = 0;
+  let page = 0;
+  let rows_per_page = 20;
 
-  let deleteLoading = false;
-  let deleteIndex;
+  let sort_column = "";
+  let decrementing = false;
+
+  let savedTempValue = "";
+  let activeModal = "";
+  let modalText = "";
+  let confirmEdit = true;
+  let exportOptions = {
+    orchestrators: [],
+    topics: {
+    action_topics: [],
+    step_topics: [],
+    observation_topics: [],
+    }
+  };
+
+  let buttonLoading = false;
+  let deleteIndex = null;
+  let updateValueData = null;
 
   let message = "";
 
@@ -32,84 +48,125 @@
     _image_sample: "gif",
     start_time: "time",
     end_time: "time",
+    id: "id",
   };
 
-  async function updateFiles() {
+  async function fetchFiles() {
     const res = await fetch(`${server}/db`);
     files = await res.json();
-    if (files) {
+    if (res.ok && files) {
       dbfile = files[0];
-      updateTables();
+      return await fetchTables();
     }
   }
 
-  async function updateTables() {
+  async function fetchTables() {
     const res = await fetch(`${server}/db/${dbfile}`);
     tables = await res.json();
     if (tables) {
       tablename = tables[0];
-      updateData();
+      return await fetchData();
     }
   }
 
-  async function updateData() {
-    const res = await fetch(`${server}/db/${dbfile}/${tablename}`);
-    tabledata = await res.json();
+  async function fetchData() {
+    message = "Fetching data...";
+    const res = await fetch(
+      `${server}/db/${dbfile}/${tablename}/${rows_per_page}/${page}?sortby=${
+        decrementing ? "-" : ""
+      }${sort_column}`
+    );
+    const result = await res.json();
+    tabledata = result["data"];
+    row_count = result["count"];
+
     columns = tabledata.length > 0 ? Object.keys(tabledata[0]) : [];
     columns_toggle = Object.fromEntries(columns.map((c) => [c, true]));
-  }
 
-  function debugPrint() {
-    console.log("data", tabledata);
+    message = "";
+    return true;
   }
 
   function saveValue(event) {
     savedTempValue = event.target.value;
   }
 
-  function confirmChange(event) {
-    if (savedTempValue != event.target.value) {
-      modalText = `Change value from ${savedTempValue} to ${event.target.value}?`;
-      updateModalActive = true;
+  async function updateValue() {
+    try {
+      const res = await fetch(
+        `${server}/db/${dbfile}/${tablename}/update/${updateValueData["id"]}?col=${updateValueData["col"]}&val=${updateValueData["value"]}`
+      );
+      message = `Updated ${updateValueData["col"]} to ${updateValueData["value"]}`;
+      activeModal = "";
+    } catch {
+      updateValueData["target"].value = savedTempValue;
+      message = `Error updating value`;
+      modalText = `Error updating value`;
+      buttonLoading = false;
     }
   }
 
   async function exportData(event) {
-    modalText = `Export data`;
     const res = await fetch(`${server}/export`);
-    exportOptions = await res.json();
-    exportModalActive = true;
+    if (res.ok) {
+      message = `Exported data successfully`;
+      activeModal = "";
+    } else {
+      message = `Error exporting data`;
+      modalText = `Error exporting data`;
+      buttonLoading = false;
+    }
   }
 
   async function deleteRow() {
     let id = tabledata[deleteIndex]["id"];
     const res = await fetch(`${server}/db/${dbfile}/${tablename}/delete/${id}`);
     if (res.ok) {
+      await fetchData();
       message = `Deleted row ${id}`;
-      tabledata.splice(deleteIndex, 1);
-      deleteLoading = false;
-      deleteModalActive = false;
+      buttonLoading = false;
+      activeModal = "";
     } else {
       message = `Error deleting row ${id}`;
       modalText = `Error deleting row ${id}`;
-      deleteLoading = false;
+      buttonLoading = false;
     }
   }
 
   function showModal(name, data = null) {
-    return () => {
-      if (name == "deleteRow") {
-        deleteModalActive = true;
-        deleteIndex = data;
-        modalText = `Delete row ${tabledata[deleteIndex]["id"]}?`;
+    return async (event) => {
+      if (name == "delete") {
+        if (confirmEdit) {
+          activeModal = "delete";
+          deleteIndex = data;
+          modalText = `Delete row ${tabledata[deleteIndex]["id"]}?`;
+        } else {
+          await deleteRow();
+        }
+      } else if (name == "update") {
+        if (savedTempValue != event.target.value) {
+          if (confirmEdit) {
+            activeModal = "update";
+            modalText = `Change value from ${savedTempValue} to ${event.target.value}?`;
+            updateValueData = data;
+            updateValueData["id"] = tabledata[updateValueData["i"]]["id"];
+            updateValueData["value"] = event.target.value;
+            updateValueData["target"] = event.target;
+          } else {
+            await updateValue();
+          }
+        }
+      } else if (name == "export") {
+        activeModal = "export";
+        modalText = `Export data`;
+        const res = await fetch(`${server}/export`);
+        exportOptions = await res.json();
       }
     };
   }
 
-  function onModalCancel(event) {
-    updateModalActive = false;
-    deleteModalActive = false;
-    exportModalActive = false;
+  function onModalCancel() {
+    activeModal = "";
   }
 
   function selectAllColumns(value) {
@@ -120,55 +177,81 @@
     };
   }
 
-  function onModalSave() {
-    console.log("save");
-    updateModalActive = false;
+  function changePage(p, force = false) {
+    return () => {
+      p = Math.min(Math.max(0, p), (row_count / rows_per_page) >> 0);
+      if (force || page != p) {
+        page = p;
+        fetchData();
+      }
+    };
   }
 
-  onMount(async function () {
-    await updateFiles();
-  });
+  function sortBy(col) {
+    return async () => {
+      decrementing = sort_column == col ? !decrementing : false;
+      sort_column = col;
+      await fetchData();
+    };
+  }
 </script>
 
-<Modal active={updateModalActive}>
+<!-- Modals -->
+<Modal active={activeModal == "update"}>
   <p>{modalText}</p>
-  <button class="button is-success" on:click={confirmChange}>Save</button>
+  <button class="button is-success" on:click={updateValue}>Save</button>
   <button class="button" on:click={onModalCancel}>Cancel</button>
 </Modal>
 
-<Modal active={deleteModalActive}>
+<Modal active={activeModal == "delete"}>
   <p>{modalText}</p>
   <button
-    class={"button is-danger" + (deleteLoading ? " is-loading" : "")}
+    class={"button is-danger" + (buttonLoading ? " is-loading" : "")}
     on:click={deleteRow}>Delete</button
   >
   <button class="button" on:click={onModalCancel}>Cancel</button>
 </Modal>
 
-<Modal active={exportModalActive}>
+<Modal active={activeModal == "export"}>
   <h3>Export to RLDS</h3>
 
-  {#each Object.keys(exportOptions) as option}
+  <div class="field is-horizontal">
+    <label class="field-label is-normal">Orchestrator</label>
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <div class="select">
+            <select>
+              {#each exportOptions["orchestrators"] as o}
+                <option>{o}</option>
+              {/each}
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {#each Object.keys(exportOptions["topics"]) as option}
     <div class="field is-horizontal">
       <label class="field-label is-normal">{option}</label>
       <div class="field-body">
-        <div class="control is-expanded">
-          <input
-            class="input is-fullwidth"
-            type="text"
-            bind:value={exportOptions[option]}
+        <div class="field is-expanded">
+          <!-- check keycodes https://www.toptal.com/developers/keycode -->
+          <Tags
+            bind:tags={exportOptions["topics"][option]}
+            addKeys={[9,32,188,13]}
+            allowPaste={true}
+            allowDrop={true}
+            onlyUnique={true}
+            placeholder={""}
+            allowBlur={true}
           />
-          <!-- <div class="select">
-            <select>
-              {#each values as item}
-                <option>{item}</option>
-              {/each}
-            </select>
-          </div> -->
         </div>
       </div>
     </div>
   {/each}
+
 
   <div class="field is-horizontal">
     <label class="field-label is-normal">Destination</label>
@@ -184,20 +267,23 @@
       </div>
     </div>
   </div>
-
-  <button class="button is-info" on:click={confirmChange}>Export</button>
+  <button
+    class={"button is-info" + (buttonLoading ? " is-loading" : "")}
+    on:click={exportData}>Export</button
+  >
   <button class="button" on:click={onModalCancel}>Cancel</button>
 </Modal>
+<!-- End Modals -->
 
 <div class="columns mt-5">
   <div class="column is-narrow">
     <h1>FogROS RT-X Dataset Viewer</h1>
   </div>
-  <div class="column">
+  <div class="column mt-3">
     <p>{message}</p>
   </div>
   <div class="column is-narrow">
-    <button class="button is-pulled-right" on:click={exportData}
+    <button class="button is-pulled-right" on:click={showModal("export")}
       >Export to RLDS</button
     >
   </div>
@@ -211,7 +297,7 @@
         <div class="field">
           <div class="control">
             <div class="select">
-              <select bind:value={dbfile} on:change={updateTables}>
+              <select bind:value={dbfile} on:change={fetchTables}>
                 {#each files as filename}
                   <option>{filename}</option>
                 {/each}
@@ -227,7 +313,7 @@
       <div class="field-body">
         <div class="control">
           <div class="select">
-            <select bind:value={tablename} on:change={updateData}>
+            <select bind:value={tablename} on:change={fetchData}>
               {#each tables as t}
                 <option>{t}</option>
               {/each}
@@ -241,12 +327,14 @@
       <label class="field-label is-bold">Features</label>
       <div class="field-body">
         <div class="field">
-          <button class="button is-small" on:click={selectAllColumns(true)}
-            >All</button
-          >
-          <button class="button is-small" on:click={selectAllColumns(false)}
-            >None</button
-          >
+          <div class="mb-2">
+            <button class="button is-small" on:click={selectAllColumns(true)}
+              >All</button
+            >
+            <button class="button is-small" on:click={selectAllColumns(false)}
+              >None</button
+            >
+          </div>
           <div class="control">
             {#each columns as col}
               <label class="checkbox">
@@ -259,17 +347,85 @@
         </div>
       </div>
     </div>
+
+    <div class="field is-horizontal">
+      <label class="field-label is-normal">Options</label>
+      <div class="field-body">
+        <label class="checkbox mt-3">
+          <input type="checkbox" bind:checked={confirmEdit} />
+          Confirm for edit/delete
+        </label>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <label class="field-label is-normal"></label>
+      <div class="field-body">
+        <input
+          class="input is-small mr-1"
+          type="input"
+          bind:value={rows_per_page}
+          on:focusout={changePage(page, true)}
+          style="width:5em"
+        />
+        per page
+      </div>
+    </div>
   </div>
+
   <div class="column table-container">
-    {#if tabledata.length > 0}
+    {#await browser ? fetchFiles() : Promise.resolve()}
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+      <progress class="progress is-normal is-light" max="100"></progress>
+    {:then done}
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <button class="button is-small" on:click={changePage(page - 1)}
+              >{"<"}</button
+            >
+            <button class="button is-small" on:click={changePage(page + 1)}
+              >{">"}</button
+            >
+          </div>
+          <div class="level-item">
+            <p>
+              Showing {1 + page * rows_per_page}-{page * rows_per_page +
+                tabledata.length} of {row_count} data points
+            </p>
+          </div>
+        </div>
+      </div>
+
       <table class="table is-striped is-narrow is-hoverable">
         <thead>
           <tr>
-            <th></th>
-            <!-- for deleting rows -->
+            <th>
+              <!-- extra first column for edittools (delete) -->
+            </th>
             {#each columns as col}
               {#if columns_toggle[col]}
-                <th>{col}</th>
+                <th
+                  class={"table-column-hover " +
+                    (sort_column == col
+                      ? decrementing
+                        ? "has-text-danger has-background-danger-light"
+                        : "has-text-info has-background-info-light"
+                      : "")}
+                  style="cursor:pointer"
+                  on:click={sortBy(col)}>{col}</th
+                >
               {/if}
             {/each}
           </tr>
@@ -281,7 +437,7 @@
                 <div
                   class="edit"
                   id={"row-" + i}
-                  on:click={showModal("deleteRow", i)}
+                  on:click={showModal("delete", i)}
                 >
                   <Fa icon={faTrashCan} color="red" style="cursor:pointer" />
                 </div>
@@ -295,19 +451,17 @@
                           <input
                             type="checkbox"
                             bind:checked={tabledata[i][col]}
-                            on:change={debugPrint}
                           />
                         </label>
+                      {:else if datatypes[col] == "id"}
+                        {val}
                       {:else if datatypes[col] == "time"}
-                        <!-- value={tabledata[i][col]} -->
                         <textarea
                           style="border: none; background: transparent; width: 100%; resize:none"
-                          on:focusin={saveValue}
-                          on:focusout={confirmChange}
+                          readonly
                           value={new Date(
                             parseInt(tabledata[i][col]) / 1000000
                           ).toISOString()}
-                          on:change={debugPrint}
                         />
                       {:else if datatypes[col] == "image"}
                         <img
@@ -324,9 +478,8 @@
                       <textarea
                         style="border: none; background: transparent; width: 100%; resize:none"
                         on:focusin={saveValue}
-                        on:focusout={confirmChange}
+                        on:focusout={showModal("update", { col: col, i: i })}
                         bind:value={tabledata[i][col]}
-                        on:change={debugPrint}
                       />
                     {/if}
                   </td>
@@ -336,9 +489,9 @@
           {/each}
         </tbody>
       </table>
-    {:else}
+    {:catch error}
       <p>Could not load data. Check your db file?</p>
-    {/if}
+    {/await}
   </div>
 </div>
 
@@ -348,5 +501,14 @@
   }
   tr:hover td .edit {
     visibility: visible;
+  }
+  .table-column-hover:hover {
+    background-color: #e8e8e8;
+    user-select: none;
+  }
+
+  :global(.svelte-tags-input-tag) {
+    background-color: #dfdfdf !important;
+    color: #000000 !important;
   }
 </style>
--- dataviewer/server/app.py
+++ dataviewer/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, make_response
+from flask import Flask, send_from_directory, request
 from flask_cors import CORS
 from glob import glob
 import dataset
@@ -24,36 +24,70 @@ def get_tables(db_name):
 
 @app.route("/db/<db_name>/<table_name>")
 @app.route("/db/<db_name>/<table_name>/<limit>")
-def get_table_from_db(db_name, table_name, limit=50):
+@app.route("/db/<db_name>/<table_name>/<limit>/<page>")
+def get_table_from_db(db_name, table_name, limit=50, page=0):
+    sortby = request.args.get("sortby")
+    order = sortby.split(",") if sortby else []
     db = dataset.connect(f"{db_connection_prefix}/{db_name}")
     table = db.get_table(table_name)
-    return list(table.all(_limit=limit))
+    query_res = db.query(f"SELECT COUNT(1) FROM {table_name}")
+    count = next(query_res)["COUNT(1)"]
+    return {
+        "count": count,
+        "data": list(
+            table.all(_limit=int(limit), _offset=int(page) * int(limit), order_by=order)
+        ),
+    }
+
 
 @app.route("/db/<db_name>/<table_name>/delete/<row_id>")
 def delete_row(db_name, table_name, row_id):
     db = dataset.connect(f"{db_connection_prefix}/{db_name}")
     table = db.get_table(table_name)
-    out = table.delete(id=int(row_id))
-    return make_response({'deleted': out}, 200)
+    deleted = table.delete(id=int(row_id))
+    return {"deleted": deleted}, 200
+
+
+@app.route("/db/<db_name>/<table_name>/update/<row_id>")
+def update_value(db_name, table_name, row_id):
+    col = request.args.get("col")
+    val = request.args.get("val")
+    if col and val:
+        db = dataset.connect(f"{db_connection_prefix}/{db_name}")
+        table = db.get_table(table_name)
+        data = {"id": int(row_id), col: val}
+        updated = table.update(data, ["id"])
+        return {"updated": updated}, 200
+    return {}, 400
 
 
 @app.route("/export")
 def export_options():
     observation_topics = ["/wrist_image", "/image", "/end_effector_state", "/state"]
     action_topics = ["/action"]
-    step_topics = ["/language_embedding", "/language_instruction", "/discount", "/reward"]
-    # orchestrator=PerTimeIntervalTopicOrchestrator(
-    #     per_step_interval=0.1,  # seconds
-    #     per_episode_interval=1.0, # seconds
-    # )
-    return {"observation_topics":observation_topics, "action_topics":action_topics, "step_topics":step_topics}
+    step_topics = [
+        "/language_embedding",
+        "/language_instruction",
+        "/discount",
+        "/reward",
+    ]
+    orchestrators = ["PerTimeIntervalTopicOrchestrator", "TestOrchestrator"]
+    return {
+        "orchestrators": orchestrators,
+        "topics": {
+            "observation_topics": observation_topics,
+            "action_topics": action_topics,
+            "step_topics": step_topics,
+        }
+    }
+
 
 @app.route("/image/<path:path>")
 def get_image(path):
-    return send_from_directory('image', path)
+    return send_from_directory("image", path)
 
 
 @app.after_request
 def after_request(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers["Access-Control-Allow-Origin"] = "*"
     return response
